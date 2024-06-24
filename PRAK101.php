133edd684868dbac1523c00cd89e640afba3db0d
elxeedkeivielen <115568456+elxeedkeivielen@users.noreply.github.com>
2024-06-25 01:39:18 +0800

    Update PRAK101.php

--- PRAK101.php
+++ PRAK101.php
@@ -1,5 +1,5 @@
 <?php
     echo "Hello World in PHP<br>";
-    print "Nama Praktikan : Hafizh Pratama Budiman <br>";
-    print_r ("NIM : 2210817310007");
+    print "Nama Praktikan : Kevin Maleakhi <br>";
+    print_r ("NIM : 2210817210031");
 ?>
